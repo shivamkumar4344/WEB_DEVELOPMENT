46cef77abb39d885083faabfc1dfa2d22f95cb4a
Shivam kumar <ydvshivam10@gmail.com>
2024-03-09 18:43:33 +0530

    This is all about basics in Express JS

---
+++ Backend/Express.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+app.get('/',(req,res)=>{
+    res.send("Hello World!");
+})
+
+app.get('/about',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
+    // res.send("about");
+    // res.json({
+    //     "Name":"Shivam",
+    //     "Age":20,
+    // });
+})
+
+app.get("/:name",(req,res)=>{
+    res.send("MY NAME IS : "+req.params.name);
+})
+
+app.listen(3000,()=>{
+    console.log(`App is listening at port 3000`);
+})
