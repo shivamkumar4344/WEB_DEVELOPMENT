364f8cec24bd25fdb6537f67ac1c2af9822d3e6d
Shivam kumar <ydvshivam10@gmail.com>
2024-03-10 13:38:54 +0530

    Completed the backed of the socket.io

--- WebSocket/index.js
+++ WebSocket/index.js
@@ -2,16 +2,26 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const http = require("http");
-const port = 9000;
-
+const port = 7000;
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+//socket io
+io.on("connection",(socket)=>{
+    socket.on("user-message",(message)=>{
+        io.emit("message",message);
+    })
+})
+
+
 app.use(express.static(path.resolve("./public")));
 app.get("/",(req,res)=>
 {
     return res.sendFile("/public/index.html");
 });
 
-server.listen(9000,()=>
+server.listen(port,()=>
 {
     console.log(`Server started at port: ${port}`);
 });
